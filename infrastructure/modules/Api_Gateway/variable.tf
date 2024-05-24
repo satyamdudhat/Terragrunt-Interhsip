d0e156f640c29f09fd55a6bd6eda30a07c1ed477
satyam <satyam.glsica19@gmail.com>
2024-05-24 11:33:17 +0530

    Update Folder Infra

---
+++ infrastructure/modules/Api_Gateway/variable.tf
@@ -0,0 +1,60 @@
+variable "api_name" {
+  description = "Name for the API Gateway"
+}
+
+
+
+variable "uri" {
+  description = "Lambda function invoke_arn"
+  type = string
+}
+
+variable "function_name" {
+  description = "Lambda fucntion Name ARN"
+  type = string
+}
+
+
+
+variable "api_path_name" {
+  type = list(string)
+}
+
+
+
+
+
+#---------------------------------- Status Methods
+variable "http_methods_status" {
+  type    = list(string)
+  default = ["GET"]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------ Employee Methods
+variable "http_methods_employee" {
+  type    = list(string)
+  default = ["GET","POST","DELETE","PATCH","PUT"]
+}
+
+
+
+
+
+#------------------------------------ Employees Methods
+variable "http_methods_employees" {
+  type    = list(string)
+  default = ["GET"]
+}
+
+
---
+++ infrastructure/modules/Lambda_Function/main.tf
@@ -0,0 +1,107 @@
+resource "aws_lambda_function" "serverless_lambda_function" {
+  function_name    = var.function_name
+  architectures = ["x86_64"]
+  package_type     = "Image"
+  image_uri        = var.image_uri
+  role             = aws_iam_role.serverless_lambda_execution_role.arn
+}
+
+
+# Lmabda Role
+resource "aws_iam_role" "serverless_lambda_execution_role" {
+  name               = var.iam_role_name
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+# IAM Policy for ECR Access or we can say that which wchi action can be do by lambda in ecr
+resource "aws_iam_policy" "serverless_lambda_ecr_access_policy" {
+  name        = var.ecr_access_policy
+  description = "Policy for granting Lambda access to ECR repositories"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect   = "Allow",
+      Action   = [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:BatchGetImage"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
+
+resource "aws_iam_policy" "serverless_dynamodb_access_policy" {
+  name        = var.dyanmodb_access_policy
+  description = "Policy for granting Lambda access to DyanmoDb Table"
+  policy      = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+})
+}
+
+
+resource "aws_iam_policy" "serverless_cloudwatch_logs_policy" {
+  name        = var.cloudwatch_access_policy
+  description = "Allows Lambda function to have full access to CloudWatch Logs"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "logs:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+
+
+
+# # Attach ECR Access Policy to IAM Role  we attcah a role with policy
+resource "aws_iam_role_policy_attachment" "lambda_ecr_access_attachment" {
+  role       = aws_iam_role.serverless_lambda_execution_role.name
+  policy_arn = aws_iam_policy.serverless_lambda_ecr_access_policy.arn
+}
+
+# Attach DynamoDb Access Policy to IAM Role  we attcah a role with Lambda Role
+resource "aws_iam_role_policy_attachment" "dyanmoDb_access_attachment" {
+  role       = aws_iam_role.serverless_lambda_execution_role.name
+  policy_arn = aws_iam_policy.serverless_dynamodb_access_policy.arn
+}
+
+
+# Attach DynamoDb Access Policy to IAM Role  we attcah a role with Lambda Role
+resource "aws_iam_role_policy_attachment" "cloudWatch_access_attachment" {
+  role       = aws_iam_role.serverless_lambda_execution_role.name
+  policy_arn = aws_iam_policy.serverless_cloudwatch_logs_policy.arn
+}
+
+
+
