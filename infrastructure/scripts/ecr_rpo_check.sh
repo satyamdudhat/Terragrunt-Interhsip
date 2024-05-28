fa647989068b781c9c50a9bdc0820331af767ee6
satyam <satyam.glsica19@gmail.com>
2024-05-28 15:04:12 +0530

    CI/CD

---
+++ infrastructure/scripts/ecr_rpo_check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if aws ecr describe-repositories --repository-names "project-dev-ecr" --profile "${AWS_PROFILE}" &> /dev/null; then
+    echo "ECR repository exists"
+    echo "::set-output name=exists::true"
+else
+    terragrunt plan
+    terragrunt apply -auto-approve -target=module.ecr_repo
+fi
