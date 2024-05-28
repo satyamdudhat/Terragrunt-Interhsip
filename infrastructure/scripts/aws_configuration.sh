b1f1668b174e68b7c03cc19c4c09580697258d57
satyam <satyam.glsica19@gmail.com>
2024-05-28 17:49:50 +0530

    CI/CD/Stage Env

--- infrastructure/scripts/aws_configuration.sh
+++ infrastructure/scripts/aws_configuration.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}" --profile "${AWS_PROFILE}"
 aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}" --profile "${AWS_PROFILE}"
 aws configure set region "${AWS_DEFAULT_REGION}" --profile "${AWS_PROFILE}"
