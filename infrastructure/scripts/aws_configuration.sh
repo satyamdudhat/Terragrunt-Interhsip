9ff4f9dcc94d4445bd7cb7170576ab7ddddc6e92
satyam <satyam.glsica19@gmail.com>
2024-05-29 11:18:15 +0530

    Update Infra

--- infrastructure/scripts/aws_configuration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}" --profile "${AWS_PROFILE}"
-aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}" --profile "${AWS_PROFILE}"
-aws configure set region "${AWS_DEFAULT_REGION}" --profile "${AWS_PROFILE}"
-echo "AWS Access Key ID: $AWS_ACCESS_KEY_ID"
