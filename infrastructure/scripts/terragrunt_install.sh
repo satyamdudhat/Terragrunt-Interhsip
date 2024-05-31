fde81c9df9c93b9d5df82c0564962ee3ec4c9f74
satyam <satyam.glsica19@gmail.com>
2024-05-31 14:52:40 +0530

    Create CI/Cd

--- infrastructure/scripts/terragrunt_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-wget https://github.com/gruntwork-io/terragrunt/releases/download/v0.45.0/terragrunt_linux_amd64
-chmod +x terragrunt_linux_amd64
-sudo mv terragrunt_linux_amd64 /usr/local/bin/terragrunt
-terragrunt --version
-terraform --version
---
+++ infrastructure/scripts/tfenv_tgenv_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#Install tfenv
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bashrc
+export PATH="$HOME/.tfenv/bin:$PATH"
+        
+
+#Install tgenv        
+git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv
+echo 'export PATH="$HOME/.tgenv/bin:$PATH"' >> ~/.bashrc
+export PATH="$HOME/.tgenv/bin:$PATH"
+source ~/.bashrc
+
+
