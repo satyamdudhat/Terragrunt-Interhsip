029c51882126ac52130026bc38ce1b9509341c66
satyam <satyam.glsica19@gmail.com>
2024-05-31 14:56:04 +0530

    Create CI/Cd

--- infrastructure/scripts/tfenv_tgenv_install.sh
+++ infrastructure/scripts/tfenv_tgenv_install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-
-#Install tfenv
+# Install tfenv
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bashrc
 export PATH="$HOME/.tfenv/bin:$PATH"
-        
 
-#Install tgenv        
+# Install tgenv        
 git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv
 echo 'export PATH="$HOME/.tgenv/bin:$PATH"' >> ~/.bashrc
 export PATH="$HOME/.tgenv/bin:$PATH"
-source ~/.bashrc
-
-
