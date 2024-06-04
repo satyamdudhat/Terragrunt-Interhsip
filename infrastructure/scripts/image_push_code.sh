a15ab051548e995cee500dc2274663becb0b43b6
satyam <satyam.glsica19@gmail.com>
2024-06-04 14:38:52 +0530

    Update a Code As Per Review

--- infrastructure/scripts/image_push_code.sh
+++ infrastructure/scripts/image_push_code.sh
@@ -4,9 +4,9 @@
 
 echo "AWS Access Key ID: $AWS_ACCOUNT_ID"
 aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" --profile "${AWS_PROFILE}" | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com
-docker build -t "${IMAGE_TAG}-Image" .
-docker tag "${IMAGE_TAG}-Image" "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/project-dev-ecr:latest
+docker build -t "${IMAGE_TAG}-image" .
+docker tag "${IMAGE_TAG}-image" "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/project-dev-ecr:latest
 docker push "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/project-dev-ecr:latest
 
-docker tag "${IMAGE_TAG}-Image" "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/project-dev-ecr:"${GITHUB_COMMIT_HASH}"
+docker tag "${IMAGE_TAG}-image" "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/project-dev-ecr:"${GITHUB_COMMIT_HASH}"
 docker push "${AWS_ACCOUNT_ID}".dkr.ecr."${AWS_DEFAULT_REGION}".amazonaws.com/project-dev-ecr:"${GITHUB_COMMIT_HASH}"
