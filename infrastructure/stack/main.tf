2040e85496472a784ece23baac32c90c42e2d4d7
satyam <satyam.glsica19@gmail.com>
2024-06-04 12:38:06 +0530

    Update a Code As Per Review

--- infrastructure/stack/main.tf
+++ infrastructure/stack/main.tf
@@ -33,7 +33,7 @@ module "ecr_repo" {
   source = "../modules/ECR"
   ecr_name = "${var.prefix}-ecr"
   image_tag_mutability = "MUTABLE"
-  imagepush_script_path = "bash ../scripts/image_push_code_master.sh"
+  imagepush_script_path = "bash ../scripts/image_push_code_dev.sh"
   providers = {
     aws = aws
   }
